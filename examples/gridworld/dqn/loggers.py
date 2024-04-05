0ad8bc1ad9130b07945acaf81a01b1cbc33232ed
TristanBester <tristanbester@gmail.com>
2024-04-05 19:24:29 +0200

    added initial JIT logger design

--- examples/gridworld/dqn/loggers.py
+++ examples/gridworld/dqn/loggers.py
@@ -1,72 +1,102 @@
-from collections import defaultdict
+from functools import partial
 
-import matplotlib.pyplot as plt
-import numpy as np
+import chex
+import jax
+import jax.numpy as jnp
+
+
+@chex.dataclass(frozen=True)
+class LoggerState:
+    """State of the logger."""
+
+    export_freq: int
+    env_count: int
+    ep_counters: chex.Array
+    ep_returns: chex.Array
+    returns_buffer: chex.Array
+    log_counter: int = 0
 
 
 class Logger:
-    def __init__(self):
-        self.metric_buffers = defaultdict(list)
+    def init(self, export_freq: int, env_count: int) -> LoggerState:
+        return LoggerState(
+            export_freq=export_freq,
+            env_count=env_count,
+            ep_counters=jnp.zeros(env_count, dtype=jnp.int32),
+            ep_returns=jnp.zeros(env_count, dtype=jnp.float32),
+            returns_buffer=jnp.zeros((env_count, export_freq), dtype=jnp.float32),
+        )
 
-    def log(self, metrics):
+    def _handle_export(self, logger_state: LoggerState) -> None:
+        """Export the data to disk."""
+        print("Exporting")
+        print(logger_state.returns_buffer)
+        print("Done")
 
-        print(metrics["reward"])
-        print(metrics["done"])
+    def _export(self, logger_state: LoggerState) -> LoggerState:
+        ep_counters = logger_state.ep_counters.at[:].set(0)
+        returns_buffer = logger_state.returns_buffer.at[:, :].set(0.0)
+        ep_returns = logger_state.ep_returns.at[:].set(0.0)
 
-        # for i, x in metrics.items():
-        #     self.metric_buffers[i].append(x)
+        updated_logger_state = logger_state.replace(
+            ep_counters=ep_counters,
+            returns_buffer=returns_buffer,
+            ep_returns=ep_returns,
+        )
 
-    def dump(self):
-        print("Dumping...")
+        jax.debug.callback(self._handle_export, logger_state)
+        return updated_logger_state
 
-        # for i, x in self.metric_buffers.items():
-        #     print(f"{i}: {x}")
+    def log(
+        self, logger_state: LoggerState, rewards: chex.Array, dones: chex.Array
+    ) -> LoggerState:
+        # Unpack the logger state
+        curr_ep_returns = logger_state.ep_returns
+        ep_counters = logger_state.ep_counters
+        returns_buffer = logger_state.returns_buffer
+        env_count = logger_state.env_count
+
+        # Compute new logger state
+        curr_ep_returns += rewards
+        dones = jax.lax.convert_element_type(dones, jnp.float32)
+        buffer_modifiers = jnp.multiply(curr_ep_returns, dones)
+        buffer_idx = (jnp.arange(4, dtype=jnp.int32), ep_counters)
+        updated_values = jnp.add(returns_buffer[buffer_idx], buffer_modifiers)
+        returns_buffer = returns_buffer.at[buffer_idx].set(updated_values)
+        ep_counters += dones.astype(jnp.int32)
+        curr_ep_returns = jnp.multiply(curr_ep_returns, jnp.subtract(1.0, dones))
+        updated_logger_state = LoggerState(
+            export_freq=logger_state.export_freq,
+            env_count=logger_state.env_count,
+            ep_counters=ep_counters,
+            ep_returns=curr_ep_returns,
+            returns_buffer=returns_buffer,
+            log_counter=logger_state.log_counter + 1,
+        )
+
+        export_required = jnp.equal(
+            jnp.mod(updated_logger_state.log_counter, updated_logger_state.export_freq),
+            0,
+        )
+
+        logger_state = jax.lax.cond(
+            export_required,
+            lambda state: self._export(state),
+            lambda state: state,
+            updated_logger_state,
+        )
+        return logger_state
 
 
-# class LogAggregator:
-#     def __init__(self):
-#         self.loggers = []
+if __name__ == "__main__":
+    logger = Logger()
+    log_state = logger.init(env_count=4, export_freq=10)
 
-#     def add_logger(self, logger):
-#         self.loggers.append(logger)
+    for i in range(20):
+        rewards = jnp.array([1, 1, 1, 1], dtype=jnp.float32)
+        dones = jnp.array([False, False, False, False], dtype=jnp.bool_)
+        log_state = logger.log(log_state, rewards, dones)
 
-#     def log(self, metrics):
-#         for logger in self.loggers:
-#             logger.log(metrics)
-
-#     def dump(self):
-#         for logger in self.loggers:
-#             logger.dump()
-
-
-class ReturnsLogger:
-    def __init__(self, env_count):
-        self.env_count = env_count
-        self.curr_ep_returns = np.zeros(env_count)
-        self.returns_buffer = {i: [] for i in range(env_count)}
-
-    def log(self, rewards, dones):
-        self.curr_ep_returns += rewards
-
-        # print(rewards)
-        # print(dones)
-        # print(self.curr_ep_returns)
-        # print()
-
-        if np.any(dones):
-            # print("Episode done")
-            done_idx = np.argwhere(dones).flatten()
-            # print(done_idx)
-            for i in done_idx:
-                self.returns_buffer[int(i)].append(float(self.curr_ep_returns[i]))
-                self.curr_ep_returns[i] = 0.0
-
-        # print(self.returns_buffer)
-        # print(self.curr_ep_returns)
-
-    def dump(self):
-        print("Dumping...")
-
-        for i, x in self.returns_buffer.items():
-            plt.plot(x)
-        plt.show()
+        rewards = jnp.array([1, 1, 1, 1], dtype=jnp.float32)
+        dones = jnp.array([True, True, True, True], dtype=jnp.bool_)
+        log_state = logger.log(log_state, rewards, dones)
